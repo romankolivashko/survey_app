f97e45dbebd348ab14d39e6541e5ef7c2b1a1bf2
Roman Kolivashko <rkolivashko@gmail.com>
2021-08-12 14:55:06 -0700

    Busimess logic work in progress

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,16 @@
 $(document).ready(function() {
-  $(".content").click(function() {
-    $(".card-showing").toggle();
-    $(".card-hidden").toggle();
+  $(".btn").click(function() {
+    $(".card-showing").hide();
+    $(".card-hidden").show();
+
+    const name = $("input#firstLasrName").val();
+    const singer = $("input#singer").val();
+    const time= $("input#time").val();
+    const dob = $("input#dob").val();
+    const color = $("input#color").val();
+
   });
 });
+
+
 
