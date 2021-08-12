8f10d149ff39cc6993aefbd443a77977588ef17d
Roman Kolivashko <rkolivashko@gmail.com>
2021-08-12 16:49:07 -0700

    Input types are validated, radio btns still need attention

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,20 @@
+
 $(document).ready(function() {
   $(".btn").click(function() {
     $(".card-showing").hide();
     $(".card-hidden").show();
 
-    const name = $("input#firstLasrName").val();
-    const singer = $("input#singer").val();
+    const name = $("input#firstLastName").val();
+    const singer = $("select#singer").val();
     const time= $("input#time").val();
-    const dob = $("input#dob").val();
+    const dob = $("input#born").val();
     const color = $("input#color").val();
+
+    $(".firstLastName").text(name);
+    $(".singer").text(singer);
+    $(".time").text(time);
+    $(".dob").text(dob);
+    $(".color").text(color);
 
   });
 });
